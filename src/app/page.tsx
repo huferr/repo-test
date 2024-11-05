5e0d20c9ad0bae1cd92780ca3a09e6198011022f
huferr <hugoferreirait@gmail.com>
2024-11-05 11:04:50 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 const constantOutside = 'it-should-fail'
-var wrongVar = ''
+var wrongVar = 'aa'
 
 export default function Home() {
 
