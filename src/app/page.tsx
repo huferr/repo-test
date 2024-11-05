d6b345b91749947292e380c322c4bb6c02d6383b
huferr <hugoferreirait@gmail.com>
2024-11-05 10:36:23 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>test</code>.
+            Get started by aaaediting <code>test</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
